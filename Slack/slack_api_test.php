4b8fcc51b8ea8fb19a43c2c3f043768f09df9b57
Cullen Brown <cjb3768@gmail.com>
2016-04-22 11:03:47 -0500

    25/Converted message call to function call
    Polling for messages is now a function; now we have more modularity

--- Slack/slack_api_test.php
+++ Slack/slack_api_test.php
@@ -9,7 +9,6 @@
 		-Multiple requests (while resulting JSON's "has more" value is still true)
 		-Determine starting timestamps
 		-Determine multiple channel id's (we have mostly used the general channel, so this may be negligible)
-		-Make all of this a function (for modularity)
 */
 
 /*
@@ -25,30 +24,36 @@ variables for this specific command
 $channelID = "C0V9109MZ"; //this is the id for our 'general' chat room; not sure where we get it, the web api test program pulled it for me
 $latestTimestamp = "1461331518.000134"; //last timestamp checked for messages; need to determine current timestamp somehow; will be needed for additional calls past the first
 $oldestTimestamp = "0"; //earliest timestamp checked for messages; can always be 0 (start of time)
-$inclusive = "1"; //a 1 means it will include messages at the latest timestamp, a 0 means it will not
+$inclusiveBounds = "1"; //a 1 means it will include messages at the latest timestamp, a 0 means it will not
 $msgCount = "1"; //a single pull can get 1 to 1000 messages; handle accordingly
 $unreadMessages = "0"; //if 1 lists how many messages in this frame are unread; not something I think we need
-$pretty = "0"; //determines how output is returned; if 1, shows more of json hierarchy, if 0, dumps result in kinda messily
+$prettyFormatting = "1"; //determines how output is returned; if 1, shows more of json hierarchy, if 0, dumps result in kinda messily
 
-/*
-html POST request code (from here: http://stackoverflow.com/questions/5647461/how-do-i-send-a-post-request-with-php)
-*/
-$url = $urlBase . $apiCommand . "?token=" . $userToken . "&channel=" . $channelID . "&latest=" . $latestTimestamp . "&oldest=" . $oldestTimestamp . "&inclusive=" . $inclusive . "&count=" . $msgCount . "&unreads=" . $unreadMessages . "&pretty=" . $pretty;
-//echo $url . "\n";
-$data = array();
+function getChatlog ($base, $command, $token, $channel, $latest, $oldest, $inclusive, $count, $unread, $pretty){
 
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
-    )
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { /* Handle error */ }
+	/*
+	html POST request code (from here: http://stackoverflow.com/questions/5647461/how-do-i-send-a-post-request-with-php)
+	*/
+	$url = $base . $command . "?token=" . $token . "&channel=" . $channel . "&latest=" . $latest . "&oldest=" . $oldest . "&inclusive=" . $inclusive . "&count=" . $count . "&unreads=" . $unread . "&pretty=" . $pretty;
+	//echo $url . "\n";
+	$data = array();
 
-var_dump($result);
-//var_dump(json_decode($result));
+	// use key 'http' even if you send the request to https://...
+	$options = array(
+    	'http' => array(
+        	'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        	'method'  => 'POST',
+        	'content' => http_build_query($data)
+    	)	
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	if ($result === FALSE) { /* Handle error */ }
+
+	//var_dump($result);
+	//var_dump(json_decode($result));
+	return $result;
+};
+
+var_dump(getChatlog($urlBase, $apiCommand, $userToken, $channelID, $latestTimestamp, $oldestTimestamp, $inclusiveBounds, $msgCount, $unreadMessages, $prettyFormatting));
 ?>
