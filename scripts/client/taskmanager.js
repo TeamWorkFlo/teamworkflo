adbb82b33078d8a497ef074af95afe88e3dec8c7
Eric Gonzalez <erigonz89@gmail.com>
2016-04-22 20:26:58 -0500

    Added task filter declaration

--- scripts/client/taskmanager.js
+++ scripts/client/taskmanager.js
@@ -1,11 +1,17 @@
 
+/* The TaskFilter is simply a function that,
+given a task object, returns true or false
+on whether that task should be returned.
+This acts as a binary classifier. */
+function TaskFilter(operation) {
+  this.passes = operation;
+}
+
+
 /* The Task Manager is the client-side source
 for tasks. It maintains the list of tasks and
 allows for filtering to retrieve them.*/
 // Task Manager
-
-// Here we can also define the task filter
-
 function TaskManager () {
   
   this.tasks = [];
@@ -14,6 +20,9 @@ function TaskManager () {
     // TODO load tasks from task aggregator
   }
   
+  /* getTasks returns all tasks available that
+  pass the provided filter rules. The parameter
+  should be of type TaskFilter to behave correctly. */
    this.getTasks = function (filter) {
      var filteredTasks = [];
      var taskCount = tasks.length;
