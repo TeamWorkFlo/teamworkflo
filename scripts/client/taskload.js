27124ed55298530413a068ccbf94e60209fb281f
Eric Gonzalez <erigonz89@gmail.com>
2016-04-23 00:13:30 -0500

    40/Data binding test

--- scripts/client/taskload.js
+++ scripts/client/taskload.js
@@ -3,11 +3,16 @@ var render = function(element) {
   var vwFilter = function(task) { return true; }
   
   var tasks = taskManager.getTasks(vwFilter);
-  d3.select(element).selectAll("p").data(tasks).enter().append("p").text("hello");
+    d3.select(element).
+    selectAll("p").
+    data(tasks).
+    enter().
+    append("p").
+    text("hello");
 };
 
 var configuration = { name:"Taskload", renderer:render };
 
 var vis = new Visualization(configuration);
 
-vis.render("#vis1");
+vis.render("#vis1-body");
