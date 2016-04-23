7e84e4e88e3e68183e78efcd3e1e8969e3f1acd8
Eric Gonzalez <erigonz89@gmail.com>
2016-04-22 23:07:34 -0500

    40/Added placeholder files for activity manager and visualization

---
+++ scripts/client/visualization.js
@@ -0,0 +1,6 @@
+
+/* The visualization type manages a specific view. In
+this view it renders one of several visualizations. The
+visualizations change depending on interactions set from
+else where, so this class responds to those changes. */
+// Visualization
