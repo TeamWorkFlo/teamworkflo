4a0b6485304a9f78370ca75557c73bbedf463ac0
Eric Gonzalez <erigonz89@gmail.com>
2016-04-22 23:25:39 -0500

    40/Added imports for javascript

--- scripts/client/visualization.js
+++ scripts/client/visualization.js
@@ -4,3 +4,8 @@ this view it renders one of several visualizations. The
 visualizations change depending on interactions set from
 else where, so this class responds to those changes. */
 // Visualization
+
+function Visualization(configuration) {
+  this.name = configuration.name;
+  this.renderVisualization = configuration.renderer;
+}
