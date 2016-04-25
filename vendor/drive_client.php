7b88dec8dfe5fb9f40f0f5bb3e31f9cd683334d1
Eric Gonzalez <erigonz89@gmail.com>
2016-04-24 21:50:17 -0500

    39/Refactored to drive_client and fixed connection to export worklog

--- vendor/drive_client.php
+++ vendor/drive_client.php
@@ -1,13 +1,13 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/autoload.php';
 
 define('APPLICATION_NAME', 'Drive API Quickstart');
 define('CREDENTIALS_PATH', '~/.credentials/drive-php-quickstart.json');
-define('CLIENT_SECRET_PATH', __DIR__ . './client_secret.json');
+define('CLIENT_SECRET_PATH', __DIR__ . '/google/client_secret.json');
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/drive-php-quickstart.json
 define('SCOPES', implode(' ', array(
-  Google_Service_Drive::DRIVE_METADATA_READONLY)
+  Google_Service_Drive::DRIVE_FILE, Google_Service_Drive::DRIVE_READONLY)
 ));
 
 if (php_sapi_name() != 'cli') {
@@ -24,7 +24,7 @@ function getClient() {
   $client->setScopes(SCOPES);
   $client->setAuthConfigFile(CLIENT_SECRET_PATH);
   $client->setAccessType('offline');
-
+  
   // Load previously authorized credentials from a file.
   $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
   if (file_exists($credentialsPath)) {
@@ -45,9 +45,11 @@ function getClient() {
     }
     file_put_contents($credentialsPath, $accessToken);
     printf("Credentials saved to %s\n", $credentialsPath);
+    
+    
   }
   $client->setAccessToken($accessToken);
-
+  
   // Refresh the token if it's expired.
   if ($client->isAccessTokenExpired()) {
     $client->refreshToken($client->getRefreshToken());
@@ -72,3 +74,37 @@ function expandHomeDirectory($path) {
 // Get the API client and construct the service object.
 $client = getClient();
 $service = new Google_Service_Drive($client);
+
+/**
+ * Parses the worklog file and returns the data vehicle for tasks
+ * @return string the JSON string with all the task entries.
+ */
+function getWorklog() {
+  $client = getClient();
+  $service = new Google_Service_Drive($client);
+  
+  $fileId = '1UUJAbPlcJ9ODmTmRNrNEG3tkGhjTF2c4WIwCG_RbKck';
+  $content = $service->files->export($fileId, 'text/csv', array(
+  'alt' => 'media' ));  
+  return $content;
+}
+
+$results = getWorklog();
+printf("%s", $results);
+
+
+// Print the names and IDs for up to 10 files.
+$optParams = array(
+  'pageSize' => 10,
+  'fields' => "nextPageToken, files(id, name)"
+);
+$results = $service->files->listFiles($optParams);
+
+if (count($results->getFiles()) == 0) {
+  print "No files found.\n";
+} else {
+  print "Files:\n";
+  foreach ($results->getFiles() as $file) {
+    printf("%s (%s)\n", $file->getName(), $file->getId());
+  }
+}
