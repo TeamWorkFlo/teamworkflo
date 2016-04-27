31dc181e30fc2f8fd163cab35d2da09c2515540a
Cullen Brown <cjb3768@gmail.com>
2016-04-26 20:00:11 -0500

    60/Starting small
    Getting back into javascript; just added some new .js files and some onclick events for index.html. Next is to render something in them (gonna go with text for now) for us to switch between.

---
+++ scripts/client/switchvis.js
@@ -0,0 +1,11 @@
+function switchSecondary(){
+    //var currText = "This is now vis1";
+    window.alert("You clicked a thing in the secondary vis!");
+    //needs to switch rendering between vis 1 and vis 2
+}
+
+function switchTertiary(){
+    //var currText = "This is now vis1";
+    window.alert("You clicked a thing in the tertiary vis!");
+    //needs to switch rendering between vis 1 and vis 3
+}
