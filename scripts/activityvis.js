d1a93a28a550cb395af6c38376e15322f66a8b84
Eric Gonzalez <erigonz89@gmail.com>
2016-04-28 12:19:40 -0500

    49/Moving toward POC with single activity thread for each user

--- scripts/activityvis.js
+++ scripts/activityvis.js
@@ -1,23 +1,41 @@
 
+
+var activityCompare = function(a,b) {
+  var aTime = a.x;
+  var bTime = b.x;
+  return bTime - aTime;
+}
+
 var render = function(element) {
   var vwFilter = function(activity) { return true; }
   var activityCallback = function (activities) {
-    
-    var seriesArr = [];
+   
+    var actorArr = [{
+      name: "Aqib Bhat",
+      data: new buckets.PriorityQueue()
+    },{
+      name: "Cullen Brown",
+      data: new buckets.PriorityQueue()
+    },{
+      name: "Eric Gonzalez",
+      data: new buckets.PriorityQueue()
+    },{
+      name: "Jorge Herrera",
+      data: new buckets.PriorityQueue()
+    }];
     $.each(activities, function(key, data) {
       var actor = data.actor;
+      
+      var actorActivity = actorArr[actor];
+      
       var source = data.source;
+      var sourceSeries = actorActivity[source];
       
-      var index = actor + "." + source;
-      var series = seriesArr[index];
-      if (!series) {
-        series = [];
-        seriesArr[index] = series;
-      }
+      var entry = {x:data.timestamp,y:source}
       
-      series.push([Date(data.timestamp), actor]);
+      sourceSeries.add(entry);
     });
-      
+    
     $(element).highcharts({
       chart: {
         zoomType: 'x'
@@ -36,7 +54,7 @@ var render = function(element) {
       legend: {
         enabled: false
       },
-      series: seriesArr
+      series: [{data:[1,2,3,5,6,1]}]
     });
   };
     
@@ -47,4 +65,4 @@ var configuration = { name:"Activity", renderer:render };
 
 var vis = new Visualization(configuration);
 
-vis.render("#vis2-body");
+vis.render("#vis1-body");
